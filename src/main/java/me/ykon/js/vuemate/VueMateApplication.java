f278db5b0b660fbd5331b9e417a57836b81e67d6
maruhachi <konchan1129@gmail.com>
2018-05-11 20:50:13 +0900

    データソースなしでの最小構成hello world

--- src/main/java/me/ykon/js/vuemate/VueMateApplication.java
+++ src/main/java/me/ykon/js/vuemate/VueMateApplication.java
@@ -2,8 +2,9 @@ package me.ykon.js.vuemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class VueMateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/me/ykon/js/vuemate/app/HomeController.java
+++ src/main/java/me/ykon/js/vuemate/app/HomeController.java
@@ -1,7 +1,15 @@
 package me.ykon.js.vuemate.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+
+        return "hello";
+    }
+
 }
